e2feca5db213e745af6a737b50c3486661dd83a0
andrey <frowzygleb@gmail.com>
2022-08-19 00:24:03 +0900

    HTTP error code changed

--- src/main/java/com/blacklog/filestorage/exception/DownloadFileException.java
+++ src/main/java/com/blacklog/filestorage/exception/DownloadFileException.java
@@ -4,6 +4,6 @@ import org.springframework.http.HttpStatus;
 
 public class DownloadFileException extends ApiException {
 	public DownloadFileException(String message) {
-		super("DOWNLOAD_FAIL", message, HttpStatus.INTERNAL_SERVER_ERROR);
+		super("DOWNLOAD_FAIL", message, HttpStatus.NOT_FOUND);
 	}
 }
