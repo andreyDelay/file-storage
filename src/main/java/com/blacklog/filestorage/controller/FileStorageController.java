1656bdb11ece617f6d4e56b57a472eb8c0a7414f
andrey <frowzygleb@gmail.com>
2022-08-19 00:23:26 +0900

    produce consume params added

--- src/main/java/com/blacklog/filestorage/controller/FileStorageController.java
+++ src/main/java/com/blacklog/filestorage/controller/FileStorageController.java
@@ -5,6 +5,7 @@ import com.blacklog.filestorage.service.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,7 +22,9 @@ public class FileStorageController {
 		return fileStorageService.saveFile(file);
 	}
 
-	@PostMapping("/download")
+	@PostMapping(value = "/download",
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
 	public Resource downloadFile(@RequestBody SavedFileInfo fileInfo) {
 		return fileStorageService.downloadFile(fileInfo);
 	}
