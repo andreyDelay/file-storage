164d1fcf058394da3d98a83a72d7ca9cf9010be3
andrey <frowzygleb@gmail.com>
2022-08-19 00:24:48 +0900

    log method added

--- src/main/java/com/blacklog/filestorage/service/FileSystemStorageService.java
+++ src/main/java/com/blacklog/filestorage/service/FileSystemStorageService.java
@@ -6,6 +6,7 @@ import com.blacklog.filestorage.exception.DownloadFileException;
 import com.blacklog.filestorage.exception.InvalidFileException;
 import com.blacklog.filestorage.exception.UploadFileException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -20,6 +21,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileSystemStorageService implements FileStorageService {
@@ -28,7 +30,9 @@ public class FileSystemStorageService implements FileStorageService {
 
 	@Override
 	public SavedFileInfo saveFile(MultipartFile multipartFile) {
+		log.info("In saveFile method.");
 		if (!isFileValid(multipartFile)) {
+			log.error("Wrong filename or extension - {}. Should be like test.xlsx", multipartFile.getOriginalFilename());
 			throw new InvalidFileException("File name is not valid.");
 		}
 
@@ -48,24 +52,29 @@ public class FileSystemStorageService implements FileStorageService {
 					.filepath(filepath.normalize().toString())
 					.build();
 		} catch (IOException e) {
+			log.error("Unexpected error, file haven't been saved. Error: {}", e.getMessage());
 			throw new UploadFileException(String.format("Couldn't save a file, error: %s", e.getMessage()));
 		}
 	}
 
 	@Override
 	public Resource downloadFile(SavedFileInfo fileInfo) {
+		log.info("In downloadFile method.");
 		if (Objects.isNull(fileInfo.getFilepath())) {
+			log.error("Error - wrong filepath.");
 			throw new DownloadFileException("The path to the file is not specified.");
 		}
 
 		try {
 			File targetFile = new File(fileInfo.getFilepath());
 			if (!targetFile.exists()) {
+				log.error("Path to a file incorrect, the file by this path not exists. Path: {}", targetFile.getPath());
 				throw new DownloadFileException(String.format("File with name - %s, not found.", fileInfo.getFilename()));
 			}
 
 			return new UrlResource(targetFile.toURI());
 		} catch (IOException e) {
+			log.error("Unexpected error. Cannot get(download) a file resource.");
 			throw new DownloadFileException(
 					String.format("Couldn't read required file. Service error: %s", e.getMessage()));
 		}
