17eac52e0c01ff4d3c95bd4b712be001b130ac89
andrey <frowzygleb@gmail.com>
2022-09-09 21:40:41 +0900

    DTO replaced by plain String var

--- src/main/java/com/blacklog/filestorage/service/FileStorageService.java
+++ src/main/java/com/blacklog/filestorage/service/FileStorageService.java
@@ -1,6 +1,5 @@
 package com.blacklog.filestorage.service;
 
-import com.blacklog.filestorage.dto.DownloadRequestDto;
 import com.blacklog.filestorage.dto.SavedFileInfo;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,5 +7,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
 	SavedFileInfo saveFile(MultipartFile multipartFile);
-	Resource downloadFile(DownloadRequestDto downloadRequest);
+	Resource downloadFile(String filepath);
 }
